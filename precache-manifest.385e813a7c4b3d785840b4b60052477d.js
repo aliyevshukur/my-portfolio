da04f2e8d812dbaba3235b09377bea1baa19e4b0
aliyevshukur <shukuralyv@gmail.com>
2021-04-01 15:34:54 +0400

    Updates

---
+++ precache-manifest.385e813a7c4b3d785840b4b60052477d.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13d4062303c44528d7b9cfcf1d4d1eeb",
+    "url": "/my-portfolio/index.html"
+  },
+  {
+    "revision": "2a971a03afece2e87094",
+    "url": "/my-portfolio/static/js/2.6455a0f9.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/my-portfolio/static/js/2.6455a0f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "19b9e22ad8216466caa1",
+    "url": "/my-portfolio/static/js/main.b82ab934.chunk.js"
+  },
+  {
+    "revision": "2df9724bfcc8b851d177",
+    "url": "/my-portfolio/static/js/runtime-main.ff45c88e.js"
+  },
+  {
+    "revision": "253d8c084205ac6f4b1b13a65a4c37d8",
+    "url": "/my-portfolio/static/media/contact-me.253d8c08.svg"
+  },
+  {
+    "revision": "3d9aef603b1d91489175d0ddf2ca525f",
+    "url": "/my-portfolio/static/media/not-found-robot.3d9aef60.svg"
+  },
+  {
+    "revision": "8ed3632623ee4605fd21b809e687579f",
+    "url": "/my-portfolio/static/media/old-pc.8ed36326.svg"
+  },
+  {
+    "revision": "4742ab03353c6c8cc3270475872e2f93",
+    "url": "/my-portfolio/static/media/web-dev.4742ab03.svg"
+  }
+]);
